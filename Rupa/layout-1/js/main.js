1e2f78ae0edb6774c9426a1297e01cae40158040
rupaodedara <rupa.odedara.fatah@gmail.com>
2020-04-15 13:49:48 +0530

    fix css,add animation,design 1 complete

--- Rupa/layout-1/js/main.js
+++ Rupa/layout-1/js/main.js
@@ -1,7 +1,11 @@
 const leftslideclip = document.querySelectorAll(".left-side-clip");
 const rightslidclip = document.querySelectorAll(".right-side-clip");
+const clip = document.querySelectorAll(".clip");
+const clip2 = document.querySelectorAll(".clip2");
 var t1 = new TimelineMax();
 
 
 t1.fromTo(leftslideclip, 2, { width:0 },{width: "20px", ease: Power2.easeInOut },"-=1.2")
-t1.fromTo(rightslidclip, 2, { width:0 },{width: "20px", ease: Power2.easeInOut },"-=2.2");
+t1.fromTo(rightslidclip, 2, { width:0 },{width: "20px", ease: Power2.easeInOut },"-=2.2")
+t1.fromTo(clip, 2, { width:0 },{width: "300px", ease: Power2.easeInOut },"-=2.2")
+t1.fromTo(clip2, 2, { width:0 },{width: "372px", ease: Power2.easeInOut },"-=2.2");
