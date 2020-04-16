0c84d0ab6d0b81995bf9121068abf6fc1eafd35f
rupaodedara <rupa.odedara.fatah@gmail.com>
2020-04-16 21:21:11 +0530

    fix svg icon,and font

--- Rupa/layout-1/js/main.js
+++ Rupa/layout-1/js/main.js
@@ -5,7 +5,7 @@ const clip2 = document.querySelectorAll(".clip2");
 var t1 = new TimelineMax();
 
 
-t1.fromTo(leftslideclip, 2, { width:0 },{width: "20px", ease: Power2.easeInOut },"-=1.2")
-t1.fromTo(rightslidclip, 2, { width:0 },{width: "20px", ease: Power2.easeInOut },"-=2.2")
+t1.fromTo(leftslideclip, 2, { width:0 },{width: "100px", ease: Power2.easeInOut },"-=1.2")
+t1.fromTo(rightslidclip, 2, { width:0 },{width: "100px", ease: Power2.easeInOut },"-=2.2")
 t1.fromTo(clip, 2, { width:0 },{width: "300px", ease: Power2.easeInOut },"-=2.2")
 t1.fromTo(clip2, 2, { width:0 },{width: "372px", ease: Power2.easeInOut },"-=2.2");
