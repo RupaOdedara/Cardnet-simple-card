6b2d3f8074578cb50455301128f344ba7b9bc14a
rupaodedara <rupa.odedara.fatah@gmail.com>
2020-04-21 15:37:05 +0530

    Layout:5

--- Rupa/layout-5/js/main.js
+++ Rupa/layout-5/js/main.js
@@ -1,3 +1,7 @@
+// fit text
+
+
+
 // right triangle
 var triangle = document.getElementById('top-triangle');
 var context = triangle.getContext('2d');
@@ -31,3 +35,44 @@ context.lineTo(10,25);
 context.fillStyle = "#FFFFFF";
 context.fill();
   
+
+
+
+
+// waves
+
+let xs = []
+for (var i = 0; i <= 500; i++) {
+  xs.push(i)
+}
+
+let t = 0
+
+function animate() {
+  
+  let points = xs.map(x => {
+    
+    let y = 200 + 8 * Math.sin((x + t) / 10)
+    
+    return [x, y]
+  })
+  
+  let path = "M" + points.map(p => {
+    return p[0] + "," + p[1]
+  }).join(" L")
+  
+  document.querySelector("path").setAttribute("d", path)
+  document.querySelector("#path2").setAttribute("d", path)
+  document.querySelector("#path3").setAttribute("d", path)
+
+  
+  t += 0.5
+  
+  requestAnimationFrame(animate)
+}
+
+animate()
+
+
+
+
