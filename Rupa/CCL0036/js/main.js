af5feb0eade30099cfffefde3db240df5455013c
rupaodedara <rupa.odedara.fatah@gmail.com>
2020-05-20 19:27:29 +0530

    set background

--- Rupa/CCL0036/js/main.js
+++ Rupa/CCL0036/js/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
-  for(let i=0; i<30; i++){
-    $('.bg-hero').prepend($('<div class="square"></div>'));
+  // for(let i=0; i<30; i++){
+  //   $('.bg-hero').prepend($('<div class="square"></div>'));
+  // }
+
+
+for(let i=0; i<32; i++){
+    $('.bg-hero').prepend($('<div class="box"></div>'));
   }
+
 });
